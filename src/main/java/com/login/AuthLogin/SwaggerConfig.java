b89c63c18fb75942fdd3ca1dd699dc4bb63135e0
gaurav <gauravbirole98@gmail.com>
2024-01-09 12:40:23 +0530

    Working fine

---
+++ src/main/java/com/login/AuthLogin/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.login.AuthLogin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.login.AuthLogin")) // Change to your base package
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger API Documentation of the get OTP Validation")
+                .description("Get the OTP for for that mobile number")
+                .version("1.0.0")
+                .contact(new Contact("Your Name", "Your Website", "Your Email"))
+                .build();
+    }
+}
